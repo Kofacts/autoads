36afcd4865f653e6cca6df37c619139e97be349e
Obodugo Rapheal <obodugorapheal@gmail.com>
2019-02-15 03:23:33 +0100

    ok ok

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,59 @@
-$(document).ready(function(){
-    $('.second_stage').hide();
-    $('.third_stage').hide();
-    $('.fourth_stage').hide();
-    var scrollTop = 0;
-    $(window).scroll(function(){
-      scrollTop = $(window).scrollTop();
-      if (scrollTop >= 100) {
-        $('#global-nav').addClass('scrolled-nav');
-      } else if (scrollTop < 100) {
-        $('#global-nav').removeClass('scrolled-nav');
-      } 
-      
-    }); 
-    
-});
-$(document).ready(function(){
+$(document).ready(function () {
+  $('.second_stage').hide();
+  $('.third_stage').hide();
+  $('.fourth_stage').hide();
   var scrollTop = 0;
-  $(window).scroll(function(){
+  $(window).scroll(function () {
     scrollTop = $(window).scrollTop();
-     $('.counter').html(scrollTop);
-    
     if (scrollTop >= 100) {
       $('#global-nav').addClass('scrolled-nav');
     } else if (scrollTop < 100) {
       $('#global-nav').removeClass('scrolled-nav');
-    } 
-    
-  }); 
+    }
+
+  });
+
+});
+$(document).ready(function () {
+  var scrollTop = 0;
+  $(window).scroll(function () {
+    scrollTop = $(window).scrollTop();
+    $('.counter').html(scrollTop);
+
+    if (scrollTop >= 100) {
+      $('#global-nav').addClass('scrolled-nav');
+    } else if (scrollTop < 100) {
+      $('#global-nav').removeClass('scrolled-nav');
+    }
+
+  });
   $('.product-options').select2();
 
-  $('.proceed1').click(function() {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
+  //TESTIMONIAL SLIDER
+  if ($(".ts-testimonial-slide").length > 0) {
+
+    $(".ts-testimonial-slide").owlCarousel({
+      autoPlay: 4000,
+      slideSpeed: 1000,
+      navigation: false,
+      pagination: true,
+      singleItem: true
+    });
+  };
+
+  $('.proceed1').click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
     $('.link1').removeClass('is-active');
     $('.first_stage').hide();
     $('.second_stage').show();
     $('.link2').addClass('is-active');
   });
-  $('.proceed2').click(function() {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
+  $('.proceed2').click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
     $('.link2').removeClass('is-active');
     $('.first_stage').hide();
     $('.second_stage').hide();
